dd924edbbb7543f0f21421fc6c6d2c3a98a4527b
JeongJinSeok <jin-seok@fxgear.net>
2017-02-23 17:32:35 +0900

    quiz.logistics center

--- src/quiz/logisticsCenter_v1/LogisticsCenterTestDrive.java
+++ src/quiz/logisticsCenter_v1/LogisticsCenterTestDrive.java
@@ -2,6 +2,11 @@ package quiz.logisticsCenter_v1;
 
 public class LogisticsCenterTestDrive {
 	public static void main(String[] args) {
+		/*
+		 * 문제점
+		 * 1) A * 10 이면?
+		 * 2) A*3 + E*1 + A*3 인경우 ?
+		 */
 		LogisticsCenter logisticsCenter = new LogisticsCenter();
 		logisticsCenter.packing("A", 3);
 		logisticsCenter.packing("B", 1);
